3224abfa2e110802314120c1fb59a4d98f5d0481
uyechi23 <uyechi23@up.edu>
2023-04-24 20:03:46 -0700

    Commented run_server.bat and updated requirements.txt

--- run_server.bat
+++ run_server.bat
@@ -1,18 +1,23 @@
-rem Bypass "Terminate Batch Job" prompt.
+rem Bypass "Terminate Batch Job" prompt upon pressing CTRL-C to stop Flask server
 if "%~1"=="-FIXED_CTRL_C" (
-   REM Remove the -FIXED_CTRL_C parameter
+   rem Remove the -FIXED_CTRL_C parameter
    SHIFT
 ) ELSE (
-   REM Run the batch with <NUL and -FIXED_CTRL_C
+   rem Run the batch with <NUL and -FIXED_CTRL_C
    CALL <NUL %0 -FIXED_CTRL_C %*
    GOTO :EOF
 )
 
+rem stop echoing command executions to terminal window
 @echo off
+
+rem clear screen
 cls
 
+rem check if a Python Virtual Environment already exists
 echo Checking for Virtual Environment Initialization...
 IF EXIST env GOTO env_exists
+rem initialize, activate, and install dependencies if it doesn't exist
 :env_not_exists
 echo Virtual Environment Not Initialized.
 echo Initializing Virtual Environment...
@@ -21,23 +26,29 @@ call env\Scripts\activate.bat
 echo Recursively Installing Dependencies...
 py -m pip install -r requirements.txt
 
+rem once the Python Virtual Environment is initialized, proceed
 :env_exists
 echo Virtual Environment Initialized!
 
+rem check if the Virtual Environment is running (VIRTUAL_ENV is set)
 IF (%VIRTUAL_ENV%=="%VIRTUAL_ENV%") GOTO :env_started
 :env_not_started
 echo Starting Virtual Environment...
 call env\Scripts\activate
 
+rem once the Python Virtual Environment is started, proceed
 :env_started
 echo Virtual Environment Started!
 
+rem install and upgrade dependencies recursively
 echo Installing Required Dependencies...
 py -m pip install -r requirements.txt
 
+rem start the Flask server
 echo Starting Flask Server...
 call flask run -h 0.0.0.0 -p 5000
 
+rem terminate the Flask server and deactivate the Virtual Environment
 echo Terminating Batch Job...
 echo Stopping Virutal Environment...
 call deactivate
